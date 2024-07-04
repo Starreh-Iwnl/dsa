38589794c1e19ca213ec450f9f18997750191bf1
Sounak Roy <iam.sounakroy@gmail.com>
2024-07-04 16:46:58 +0530

    Valid Anagram

---
+++ dsa/leetcode/242.py
@@ -0,0 +1,27 @@
+""" Leetcode #242
+    Valid Anagram
+    
+    https://leetcode.com/problems/valid-anagram/
+"""
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        cnt = {}
+        
+        for x in s:
+            if x in cnt:
+                cnt[x] += 1
+            else:
+                cnt[x] = 1
+        
+        for x in t:
+            if x in cnt:
+                cnt[x] -= 1
+            else:
+                cnt[x] = 1
+        
+        for val in cnt.values():
+            if val != 0:
+                return False
+        
+        return True
