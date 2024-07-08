3a08c243cd50d310d3187c08d9ce6c49d66ae934
Sounak Roy <iam.sounakroy@gmail.com>
2024-07-08 16:48:08 +0530

    Right side view of a binary tree

---
+++ dsa/leetcode/199.py
@@ -0,0 +1,35 @@
+""" Leetcode #199
+    Binary Tree : Right Side View
+
+    https://leetcode.com/problems/binary-tree-right-side-view/
+"""
+
+# Definition for a binary tree node
+
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from collections import deque
+
+class Solution:
+    def rightSideView(self, root: Node) -> list[int]:
+        res = []
+        q = deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        
+        return res
