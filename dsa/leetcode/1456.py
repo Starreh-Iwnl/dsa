b20bcdbc1ed86b279f287efb8b829def7d7696ab
Sounak Roy <iam.sounakroy@gmail.com>
2024-07-08 17:56:23 +0530

    Maximum number of vovels in a substring of given length

--- dsa/leetcode/1456.py
+++ dsa/leetcode/1456.py
@@ -1,5 +1,5 @@
 """ Leetcode #1456
-    Maximum number of vovels in a substring of given length
+    Maximum number of vowels in a substring of given length
 
     https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
 """
@@ -7,15 +7,15 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         l, r, curr, res = 0, 0, 0, 0
-        vovelSet = {"a", "e", "i", "o", "u"}
+        vowel = {"a", "e", "i", "o", "u"}
 
         while r < len(s):
 
-            if s[r] in vovelSet:
+            if s[r] in vowel:
                 curr += 1
             
             if (r - l + 1) > k:
-                if s[l] in vovelSet:
+                if s[l] in vowel:
                     curr -= 1
                 l += 1  
             
